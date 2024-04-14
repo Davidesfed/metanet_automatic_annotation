f5f88649ac426e2c05236f7bc69846a4e3166e71
Davide <davise.mauri998@hotmail.it>
2024-04-14 13:56:18 +0200

    fatto ancestor list build con depth

--- buildLUs.py
+++ buildLUs.py
@@ -6,73 +6,52 @@ import requests
 import re
 import os
 
-def retrieve_lus_from(data_source, metanet_class, metanet_frames):
+def retrieve_lus_from(data_source, metanet_frame):
     if data_source.lower() == "metanet":
-        return retrieve_lus_from_metanet(metanet_class, metanet_frames)
+        return retrieve_lus_from_metanet(metanet_frame)
     elif data_source.lower() == "framenet":
-        return retrieve_lus_from_framenet(metanet_class, metanet_frames)
+        return retrieve_lus_from_framenet(metanet_frame)
     elif data_source.lower() == "wordnet":
-        return retrieve_lus_from_wordnet(metanet_class, metanet_frames)
+        return retrieve_lus_from_wordnet(metanet_frame)
     elif data_source.lower() == "conceptnet":
-        return retrieve_lus_from_conceptnet(metanet_class, metanet_frames)
+        return retrieve_lus_from_conceptnet(metanet_frame)
 
-def retrieve_lus_from_metanet(metanet_class, metanet_frames):
-    lexical_units = {"source": set(), "target": set()}
-    inserted = 0
-    for frame in metanet_frames:
-        if frame['frame'] == metanet_class["source frame"]:
-            no_pos_lus = [re.sub(r'\.(n|r|v|a)', '', x) for x in frame["lus"]]
-            lexical_units["source"].update(no_pos_lus)
-            inserted += 1
-        if frame['frame'] == metanet_class["target frame"]:
-            no_pos_lus = [re.sub(r'\.(n|r|v|a)', '', x) for x in frame["lus"]]
-            lexical_units["target"].update(no_pos_lus)
-            inserted += 1
-        if inserted == 2:
-            break
+def retrieve_lus_from_metanet(metanet_frame):
+    lexical_units = [re.sub(r'\.(n|r|v|a)', '', x) for x in metanet_frame["lus"]]
+    return lexical_units
 
-    return {"source": list(lexical_units["source"]), "target": list(lexical_units["target"])}
+def retrieve_lus_from_framenet(metanet_frame):
+    lexical_units = set()
+    for fn_frame_name in metanet_frame['relevant_fn_frames']:
+        try:
+            fn_frame = fn.frame(fn_frame_name)
+            no_pos_lus = [re.sub(r'\.(n|r|v|a)', '', x) for x in fn_frame.lexUnit]
+            lexical_units.update(no_pos_lus)
+        except FramenetError:
+            print(f"FrameNet error with frame {fn_frame_name}")
+    return list(lexical_units)
 
-def retrieve_lus_from_framenet(metanet_class, metanet_frames):
-    lexical_units = {"source": set(), "target": set()}
-    for frame in metanet_frames:
-        for key in ["source", "target"]:
-            if frame['frame'] == metanet_class[f"{key} frame"]:
-                for fn_frame_name in frame['relevant_fn_frames']:
-                    try:
-                        fn_frame = fn.frame(fn_frame_name)
-                        no_pos_lus = [re.sub(r'\.(n|r|v|a)', '', x) for x in fn_frame.lexUnit]
-                        lexical_units[key].update(no_pos_lus)
-                    except FramenetError:
-                        print(f"FrameNet error with frame {fn_frame_name}")
-    return {"source": list(lexical_units["source"]), "target": list(lexical_units["target"])}
+def retrieve_lus_from_wordnet(metanet_frame):
+    lexical_units = set()
+    synsets = wn.synsets(metanet_frame["frame"])
+    if len(synsets) != 0:
+        for word in synsets[0].lemmas():
+            lexical_units.add(word.name().split('.')[0])
+    return list(lexical_units)
 
-def retrieve_lus_from_wordnet(automatic_annotation, metanet_frames):
-    lexical_units = {"source": set(), "target": set()}
-    for key in ["source", "target"]:
-        synsets = wn.synsets(automatic_annotation[key + " frame"])
-        if len(synsets) != 0:
-            for word in synsets[0].lemmas():
-                lexical_units[key].add(word.name().split('.')[0])
-    return {"source": list(lexical_units["source"]), "target": list(lexical_units["target"])}
-
-def retrieve_lus_from_conceptnet(automatic_annotation, metanet_frames):
-    lexical_units = {"source": set(), "target": set()}
-    for key in ["source", "target"]:
-        lemma = automatic_annotation[key + " frame"].lower()
-        lexical_units[key].add(lemma)
-        node_id = "/c/en/" + lemma.replace(" ", "_")
-        url = f'http://api.conceptnet.io/query?node={node_id}&other=/c/en'
-        concepts = requests.get(url).json()
-        for edge in concepts["edges"]:
-            for k in ['start', 'end']:
-                word = edge[k]["@id"].split('/')[3].replace("_", " ")
-                lexical_units[key].add(word)
-    return {"source": list(lexical_units["source"]), "target": list(lexical_units["target"])}
+def retrieve_lus_from_conceptnet(metanet_frame):
+    lexical_units = set()
+    lemma = metanet_frame["frame"].lower()
+    node_id = "/c/en/" + lemma.replace(" ", "_")
+    url = f'http://api.conceptnet.io/query?node={node_id}&other=/c/en'
+    concepts = requests.get(url).json()
+    for edge in concepts["edges"]:
+        for key in ['start', 'end']:
+            word = edge[key]["@id"].split('/')[3].replace("_", " ")
+            lexical_units.add(word)
+    return list(lexical_units)
 
 def build_lexical_units_file():
-    with open("data/metanet_classes.jsonl", "r", encoding='utf8') as f:
-        metanet_classes = [json.loads(line) for line in f.readlines()]
     with open("data/metanet_frames.jsonl", "r", encoding='utf8') as f:
         metanet_frames = [json.loads(line) for line in f.readlines()]
 
@@ -81,37 +60,40 @@ def build_lexical_units_file():
             print("File already exists, overwriting it")
 
     i = 0
-    for metanet_class in metanet_classes:
-        print(f'Processing {metanet_class["metaphor"]}, ({i}/{len(metanet_classes)})')
+    n = len(metanet_frames)
+    for metanet_frame in metanet_frames:
+        print(f'Processing {metanet_frame["frame"]}, ({i}/{n})')
         i += 1
-        if not all([metanet_class["source frame"], metanet_class["target frame"]]):
-            continue
         lexical_units = {
-            "category": metanet_class["metaphor"],
-            "source frame": metanet_class["source frame"],
-            "target frame": metanet_class["target frame"],
+            "frame": metanet_frame["frame"],
+            "ancestors": set(),
             "lus": {
-                "metanet": {
-                    "source": [],
-                    "target": []
-                }, "framenet": {
-                    "source": [],
-                    "target": []
-                }, "wordnet": {
-                    "source": [],
-                    "target": []
-                }, "conceptnet": {
-                    "source": [],
-                    "target": []
-                }
+                "metanet": [], 
+                "framenet": [], 
+                "wordnet": [],
+                "conceptnet": []
             }
         }
 
-        for data_source in  ["metanet", "framenet", "wordnet", ]:
-            lexical_units["lus"][data_source] = retrieve_lus_from(data_source, metanet_class, metanet_frames)
+        for data_source in  ["metanet", "framenet", "wordnet",]:
+            lexical_units["lus"][data_source] = retrieve_lus_from(data_source, metanet_frame)
         
-        lexical_units["source frame"] = metanet_class["source frame"]
-        lexical_units["target frame"] = metanet_class["target frame"]
+        frame = metanet_frame.copy()
+        depth = 1
+        ancestors = [(x,depth) for x in metanet_frame["subcase of"]]
+        while len(ancestors) > 0:
+            ancestor_name, last_depth = ancestors.pop(0)
+            if (ancestor_name, last_depth) in lexical_units["ancestors"]:
+                continue
+            depth = last_depth + 1
+            lexical_units["ancestors"].add((ancestor_name, depth))
+            for x in metanet_frames:
+                if x["frame"] == ancestor_name:
+                    frame = x
+                    break
+            ancestors.extend([(x,depth) for x in frame["subcase of"]])
+        lexical_units["ancestors"] = list(lexical_units["ancestors"])
+
         with open("data/lexical_units.jsonl", "a", encoding='utf8') as f:
             f.write(json.dumps(lexical_units) + '\n')
 
