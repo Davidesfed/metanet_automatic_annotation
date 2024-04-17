07b35f614dcffd7328350d797abee52f2e3ef9a3
Davide <davise.mauri998@hotmail.it>
2024-04-17 18:56:37 +0200

    Valutazione funzionante + run

--- evaluation.py
+++ evaluation.py
@@ -32,6 +32,7 @@ def update_stats(stats, manual_annotation, automatic_annotation):
     label = compute_label(manual_annotation, automatic_annotation)
     lb = label.replace(' ', '_')
     if label == 'true positives':
+        stats[label]['total'] += 1
         flag = 0
         for cand_source in automatic_annotation["source"].split("/"):
             for cand_target in automatic_annotation["target"].split("/"):
@@ -49,15 +50,17 @@ def update_stats(stats, manual_annotation, automatic_annotation):
         stats[label] += 1
         with open(f'data/results/{lb}.jsonl', 'a', encoding='utf8') as f:
             f.write(json.dumps(automatic_annotation) + "\n")
+    return stats
 
 def print_stats(stats):
-    total = stats["true positives"]["total"] + stats["true negatives"] + stats["false positives"] + stats["false negatives"]
+    total = stats['true positives']['total']
+    total += sum([stats[key] for key in stats.keys() if key not in ['true positives','skipped', 'data_sources']])
     print(f"Number of metaphors analyzed: {total}/{total + stats['skipped']}")
     print(f'Data sources used: {", ".join(stats["data_sources"])}')
     print(f'True positives: {stats["true positives"]["total"]}, of which EQUAL: {stats["true positives"]["equal"]} and DIVERSE: {stats["true positives"]["diverse"]}')
     for key in stats.keys():
         if key not in ['true positives','skipped', 'data_sources']:
-            print(f'{key}: {stats[key]}')
+            print(f'{key.capitalize()}: {stats[key]}')
 
 
 def main():
@@ -80,25 +83,28 @@ def main():
 
     with open('data/metanet_manual_annotations.json', 'r', encoding='utf8') as f:
         manual_annotations = json.load(f)
-    with open('data/metanet_automatic_annotation.json', 'r', encoding='utf8') as f:
+    with open('data/metanet_automatic_annotations.json', 'r', encoding='utf8') as f:
         automatic_annotations = json.load(f)
-        data_sources = automatic_annotations.pop(0)['data_sources']
+        stats['data_sources'] = automatic_annotations.pop(0)['data_sources']
         
     for key in stats.keys():
-        if key not in ['skipped', 'data_sources']:
-            name = key.replace(' ', '_')
-            print(f"Pulizia file {name}")
-            #with open(f'data/results/{name}.jsonl', 'w', encoding='utf8') as f:
-                #pass
+        name = key.replace(' ', '_')
+        with open(f'data/results/{name}.jsonl', 'w', encoding='utf8') as f:
+            pass
     
-    for manual_annotation in manual_annotations:
-        for automatic_annotation in automatic_annotations:
+    for i in range(len(manual_annotations)):
+        for j in range(len(automatic_annotations)):
+            manual_annotation = manual_annotations[i]
+            automatic_annotation = automatic_annotations[j]
             if manual_annotation['category'] == automatic_annotation['category']:
                 stats = update_stats(stats, manual_annotation, automatic_annotation)
                 break
+            elif j > i:
+                stats['skipped'] += 1
+                with open(f'data/results/skipped.jsonl', 'a', encoding='utf8') as f:
+                    f.write(json.dumps(manual_annotation) + "\n")
+                break
 
-            print(f'Error: categories {manual_annotation["category"]} and {automatic_annotation["category"]} do not match')
-            continue
         
     print_stats(stats)
 
