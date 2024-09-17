265c313af18d8919cbf21c8416717ae17c4e0bbd
Davide <davise.mauri998@hotmail.it>
2024-09-17 21:32:49 +0200

    Parametrized set of labels

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,8 @@
 import json
 
 EVAL_DIR = 'data/aut_results'
+# LABEL_SET = "expanded"
+LABEL_SET = "standard"
 
 def load_data():
     with open('data/metanet_manual_annotations.json', 'r', encoding='utf8') as f:
@@ -21,29 +23,32 @@ def clear_output_files(stats):
 
 def compute_label(manual_annotation, automatic_annotation):
     label = ""
+    # Comments contain an example to illustrate what is happening
     label_dict = {
-        'source positives': automatic_annotation['source'] != '',
-        'source negatives': automatic_annotation['source'] == '',
-        'target positives': automatic_annotation['target'] != '',
-        'target negatives': automatic_annotation['target'] == '',
-        'true source': (automatic_annotation['source'] != '') == (manual_annotation['source'] != ''), # iff implementation
-        'false source': (automatic_annotation['source'] != '') == (manual_annotation['source'] == ''),
-        'true target': (automatic_annotation['target'] != '') == (manual_annotation['target'] != ''),
-        'false target': (automatic_annotation['target'] != '') == (manual_annotation['target'] == ''),
+        'source positives': automatic_annotation['source'] != '', # True
+        'source negatives': automatic_annotation['source'] == '', # False
+        'target positives': automatic_annotation['target'] != '', # False
+        'target negatives': automatic_annotation['target'] == '', # True
+        'true source': (automatic_annotation['source'] != '') == (manual_annotation['source'] != ''), # True
+        'false source': (automatic_annotation['source'] != '') == (manual_annotation['source'] == ''), # False
+        'true target': (automatic_annotation['target'] != '') == (manual_annotation['target'] != ''), # False
+        'false target': (automatic_annotation['target'] != '') == (manual_annotation['target'] == ''), # True
     }
-    label_dict = dict(filter(lambda x: x[1], label_dict.items()))
+    label_dict = dict(filter(lambda x: x[1], label_dict.items())) # ['source positives', 'target negatives', 'true source', 'false target']
     labels = []
     for key1 in label_dict.keys():
         for key2 in label_dict.keys():
-            if key1[-6:] == key2[:6]:
-                l = key1.split(' ')
-                l.append(key2[7:])
-                labels.append(l)
-    if labels[0][0] == labels[1][0] and labels[0][2] == labels[1][2]:
+            if key1[-6:] == key2[:6]: # key1='true source' & key2='source positives' OR key1='false target' & key2='target negatives'
+                l = key1.split(' ') # ['true', 'source'] OR ['false', 'target']
+                l.append(key2[7:]) # ['true', 'source', 'positives'] OR ['false', 'target', 'negatives']
+                labels.append(l) # labels=[['true', 'source', 'positives'], ['false', 'target', 'negatives']]
+    if labels[0][0] == labels[1][0] and labels[0][2] == labels[1][2]: # False
         label = " ".join([labels[0][0], labels[0][2]])
     else: 
-        label = list(filter(lambda x: 'positives' in x, labels))
-        label = " ".join(label[0])
+        label = list(filter(lambda x: 'negatives' in x, labels))[0] # label=['false', 'target', 'negatives']
+        if LABEL_SET != "expanded":
+            label.pop(1) # label=['false', 'negatives']
+        label = " ".join(label) # label='false target negatives' OR 'false negatives'
     return label
 
 def update_stats(stats, manual_annotation, automatic_annotation):
@@ -58,7 +63,7 @@ def update_stats(stats, manual_annotation, automatic_annotation):
         stats[label]['total'] += 1
         for cand_source in automatic_annotation["source"].split("/"):
             for cand_target in automatic_annotation["target"].split("/"):
-                if manual_annotation["source"].lower() == cand_source and manual_annotation["target"].lower() == cand_target:
+                if cand_source in manual_annotation["source"].lower() and cand_target in manual_annotation["target"].lower():
                     stats["true positives"]["equal"] += 1
                     with open(f'{EVAL_DIR}/{lb}_equal.jsonl', 'a', encoding='utf8') as f:
                         f.write(json.dumps(automatic_annotation) + "\n")
@@ -92,13 +97,15 @@ def main():
         'false positives': 0,
         'true negatives': 0,
         'false negatives': 0,
-        'true source positives': 0,
-        'true target positives': 0,
-        'false source positives': 0,
-        'false target positives': 0,
         'skipped': 0,
         'data_sources': ''
     }
+
+    if LABEL_SET == "expanded":
+        stats['false source negatives'] = 0
+        stats['false target negatives'] = 0
+        stats['true source negatives'] = 0
+        stats['true target negatives'] = 0
 
     manual_annotations, automatic_annotations, stats['data_sources'] = load_data()
     clear_output_files(stats)
